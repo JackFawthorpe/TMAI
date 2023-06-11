73cff31696d3b3122b9e0e7ffd407b99c5021a17
Jack <fawthorp878@gmail.com>
2023-06-11 12:50:10 +1200

    Initial Commit

---
+++ frontend/src/Pages/Home.svelte
@@ -0,0 +1,20 @@
+<script>
+    import {onMount} from "svelte";
+    import {API} from "../apis/API.js";
+
+    let testString = "Loading";
+
+    onMount(() => {
+        API.get(false).then((data) => {
+            console.log(data);
+            testString = data;
+        }).catch((error) => {
+            console.log(error)
+        });
+    })
+</script>
+
+<div>
+    <h1>Body</h1>
+    <h3>{testString}</h3>
+</div>
---
+++ frontend/src/components/Navbar.svelte
@@ -0,0 +1,7 @@
+<script>
+
+</script>
+
+<div>
+    <h1>NavBar</h1>
+</div>
