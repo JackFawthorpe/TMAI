5c6462ae4f24de229edb3d0501b8cd61ec07dfac
Jack Fawthorpe <fawthorp878@gmail.com>
2023-06-14 21:45:20 +1200

    Infra: Removed template code and cleaned front end

--- frontend/src/Pages/Home.svelte
+++ frontend/src/Pages/Home.svelte
@@ -1,28 +1,15 @@
 <script>
-    import {onMount} from "svelte";
-    import {API} from "../apis/API.js";
 
-    let testString = "Loading";
-
-    onMount(() => {
-        API.get(false).then((data) => {
-            console.log(data);
-            testString = data;
-        }).catch((error) => {
-            console.log(error)
-        });
-    })
 </script>
 
 <div>
-<!--    <h1>Body</h1>-->
-<!--    <h3>{testString}</h3>-->
     <div class="container p-5">
         <div class="row">
             <div class="col">
                 <button class="bg-indigo-800 text-green-50 py-1 px-3 text-center rounded">
                     Button
                 </button>
+                <h1 class="text-indigo-800 font-bold text-5xl">Lorem Dipsum Colore</h1>
             </div>
         </div>
     </div>
