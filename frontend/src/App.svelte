e79e6e4ffd6a78575e7084607714ec5fe41d568e
Jack Fawthorpe <fawthorp878@gmail.com>
2023-06-14 20:31:09 +1200

    Infra: Added tailwind-css

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,6 +3,7 @@
   import Home from './pages/Home.svelte'
   import Navbar from "./components/Navbar.svelte";
   import Footer from "./components/Footer.svelte";
+  import "./app.css"
 </script>
 
 <Router>
