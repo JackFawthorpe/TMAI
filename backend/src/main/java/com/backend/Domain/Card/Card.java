0e20c42bdf7d54efc1e9e8c17b159852b204cb87
Emma Fawthorpe <emmacansew@gmail.com>
2023-06-16 16:36:04 +1200

    Card CSV Reader

--- backend/src/main/java/com/backend/Domain/Card/Card.java
+++ backend/src/main/java/com/backend/Domain/Card/Card.java
@@ -7,6 +7,10 @@ public class Card {
 
     private String title;
 
+    public Card(String title){
+        this.title = title;
+    }
+
     public String getTitle() {
         return this.title;
     }
