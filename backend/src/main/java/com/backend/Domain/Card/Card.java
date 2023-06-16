2ddee543332f43bb7b7151d602a5bec100376e7c
Emma Fawthorpe <emmacansew@gmail.com>
2023-06-17 00:46:05 +1200

    Backend: Added Tags to Cards

--- backend/src/main/java/com/backend/Domain/Card/Card.java
+++ backend/src/main/java/com/backend/Domain/Card/Card.java
@@ -1,11 +1,16 @@
 package com.backend.Domain.Card;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Entity to represent a playable card in the game
  */
 public class Card {
 
     private String title;
+
+    private List<Tag> tags = new ArrayList<>();
 
     private int cost = -1;
 
@@ -29,4 +34,11 @@ public class Card {
 
     public void setTitle(String title) {this.title = title;}
 
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
 }
---
+++ backend/src/main/java/com/backend/Domain/Card/Tag.java
@@ -0,0 +1,18 @@
+package com.backend.Domain.Card;
+
+/**
+ * An Enum containing the names of each tag
+ */
+public enum Tag {
+    BUILDING,
+    SPACE,
+    SCIENCE,
+    PLANT,
+    MICROBE,
+    ANIMAL,
+    POWER,
+    JOVIAN,
+    EARTH,
+    CITY,
+    EVENT
+}
