50dc508f7130c08660478a9996025145a0165a93
Emma Fawthorpe <emmacansew@gmail.com>
2023-06-16 20:29:24 +1200

    Cost added to cards

--- frontend/src/Components/CardCard.svelte
+++ frontend/src/Components/CardCard.svelte
@@ -2,8 +2,12 @@
     export let card: Card;
 </script>
 
-<div>
-    <div class="p-6 bg-white border border-gray-200 rounded-lg shadow-xl min-h-[150px]">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{card.title}</h5>
+<div class="p-6 bg-white border border-gray-200 rounded-lg shadow-xl min-h-[150px]">
+    <div class="flex justify-between">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 flex-shrink">{card.title}</h5>
+        <div class="relative flex-none float-right h-12 w-12">
+            <img src="src/Resources/Credit.png" class="object-cover h-12 w-12" alt="credit"/>
+            <h1 class="absolute text-2xl text-black font-bold top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">{card.cost}</h1>
+        </div>
     </div>
 </div>
