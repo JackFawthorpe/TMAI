baefc9adf6f85bc72d502deef258c419f692e475
Jack Fawthorpe <fawthorp878@gmail.com>
2023-06-21 13:28:43 +1200

    Fix: Fixed issues with cached tailwind styling meaning dynamic tailwind classes broke

--- frontend/src/Components/Game/BoardCard.svelte
+++ frontend/src/Components/Game/BoardCard.svelte
@@ -19,13 +19,16 @@
         translateX(${Math.abs((MAIN_ROW_COUNT - 1)/2 -index) * (CELL_SIZE) / 2 + BOARD_HORZ_SHIFT}px)
         translateY(${index * VERT_ROW_SHIFT + BOARD_VERT_SHIFT}px)`
     }
+    const imageStyle = `height: ${CELL_SIZE}px; width: ${CELL_SIZE}px`
+    const boardStyle = `height: ${BOARD_Y_SIZE}px; width: ${BOARD_X_SIZE}px`
+
 </script>
 
-<div class="h-[{BOARD_Y_SIZE}px] w-[{BOARD_X_SIZE}px] bg-white rounded-xl">
+<div class="bg-white rounded-xl" style={boardStyle}>
     {#each board.cells as row, i}
         <div class="flex absolute" style={getTransform(i)}>
             {#each row as i}
-                <img src={imageArray[0]} alt="cell" class="h-[{CELL_SIZE}px] w-[{CELL_SIZE}px]">
+                <img src={imageArray[0]} style={imageStyle} alt="cell">
             {/each}
         </div>
     {/each}
