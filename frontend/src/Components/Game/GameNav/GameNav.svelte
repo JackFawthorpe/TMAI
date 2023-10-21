c62dcd8a1a26ae48e2801b33368a58628c42a931
Jack Fawthorpe <fawthorp878@gmail.com>
2023-10-21 15:30:48 +1300

    Feature: Added Tab System to jump between players and cards tabs

--- frontend/src/Components/Game/GameNav/GameNav.svelte
+++ frontend/src/Components/Game/GameNav/GameNav.svelte
@@ -2,11 +2,33 @@
     import SocketConnectionDisplay from "../SocketConnectionDisplay.svelte";
     import PlayerBrowser from "../PlayerBrowser.svelte";
 
+    let currentTab: 'Players' | 'Cards' = 'Players';
+
     export let connected: boolean;
+
 </script>
 
 <div class="w-[40%] bg-white flex flex-col justify-between">
-    <PlayerBrowser/>
+    <div class="tab-nav">
+        {#each ["Players", "Cards"] as tab, index}
+            <div class="tab"
+                 on:click={() => {currentTab = tab}}
+                 class:tab-active={currentTab === tab}>
+                <span class="tab-text">
+                    {tab}
+                </span>
+            </div>
+        {/each}
+    </div>
+    <div class="flex-grow">
+        {#if currentTab === 'Players'}
+            <PlayerBrowser/>
+        {:else if currentTab === 'cards'}
+            <h3>Test</h3>
+        {:else}
+            <h3>Blessed</h3>
+        {/if}
+    </div>
     <div>
         <SocketConnectionDisplay connected={connected}/>
     </div>
